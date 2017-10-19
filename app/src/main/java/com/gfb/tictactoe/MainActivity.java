2862bc9736c22def6f542634d332d6ef3fd2a0ac
gabriel022 <bettipb@gmail.com>
2017-10-18 23:43:08 -0700

    2

--- app/src/main/java/com/gfb/tictactoe/MainActivity.java
+++ app/src/main/java/com/gfb/tictactoe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gfb.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button playButton;
+    Button settingsButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        playButton = (Button) findViewById(R.id.playButton);
+        playButton.setOnClickListener(this);
+    //    settingsButton = (Button) findViewById(R.id.settingsButton);
+      //  settingsButton.setOnClickListener(this);
+
+
+
 
        // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
      //   fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +42,27 @@ public class MainActivity extends AppCompatActivity {
        //     }
       //  });
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.playButton:
+                //DO something
+                Intent y;
+                y = new Intent(this, GameActivity.class);
+                startActivity(y);
+                break;
+            case R.id.settingsButton:
+                Intent z;
+                z = new Intent(this,SettingsActivity.class);
+                startActivity(z);
+                break;
+
+
+
+    }
+        }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,13 +76,14 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+      //  int id = item.getItemId();
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
         return super.onOptionsItemSelected(item);
+        //noinspection SimplifiableIfStatement
+       // if (id == R.id.action_settings) {
+         //   return true;
+       // }
+
     }
 }
